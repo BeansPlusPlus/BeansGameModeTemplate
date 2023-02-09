1172ac16001411211f21c209e1adf0447f8525ca
Tomay0 <thomaspage33@gmail.com>
2023-02-09 21:00:59 +1300

    Update template plugin

--- src/main/java/beansplusplus/TemplateGame.java
+++ src/main/java/beansplusplus/TemplateGame.java
@@ -1,6 +1,8 @@
 package beansplusplus;
 
-import beansplusplus.gameconfig.GameConfiguration;
+import beansplusplus.beansgameplugin.Game;
+import beansplusplus.beansgameplugin.GameConfiguration;
+import beansplusplus.beansgameplugin.GameState;
 import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -9,23 +11,33 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Scoreboard;
 
 import org.bukkit.scoreboard.Team;
 
-public class Game implements Listener {
+// Template game that extends a Game and listens for events
+public class TemplateGame implements Game, Listener {
+  private TemplatePlugin plugin;
+  private GameState state;
 
-  private Plugin plugin;
+  private int someNum;
 
-  public Game(TemplatePlugin plugin) {
+  public TemplateGame(TemplatePlugin plugin, GameConfiguration config, GameState state) {
+    // You will need to keep track of the plugin to setup tasks and event listeners
     this.plugin = plugin;
+
+    // you should keep track of the game state to apply logic whenever the game is paused or to end the game
+    this.state = state;
+
+    // get a value from the configuration
+    this.someNum = config.getValue("some_number");
   }
 
+  @Override
   public void start() {
-    double num = GameConfiguration.getConfig().getValue("some_number");
+    // TODO: start the game. This does some basic setup for the game and registers listeners
     for (Player player : Bukkit.getOnlinePlayers()) {
       player.setHealth(20);
       player.setLevel(0);
@@ -37,18 +49,31 @@ public class Game implements Listener {
     World world = Bukkit.getWorld("world");
     world.setTime(1000);
 
+    // You will almost certainly need to register a listener. This is how you do it:
     Bukkit.getServer().getPluginManager().registerEvents(this, plugin);
+    Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, this::repeatingTask, 20, 20);
     refreshScoreboard();
   }
 
-  public void end() {
-    HandlerList.unregisterAll(this);
 
-    for (Player player : Bukkit.getOnlinePlayers()) {
-      player.setGameMode(GameMode.SPECTATOR);
-    }
+  @Override
+  public void cleanUp() {
+    // TODO: whatever needs to be done to stop the game from running.
+    // Do not call this method within your game.
+    // Use state.gameStop() to end the game and this will be called as a result.
+    HandlerList.unregisterAll(this);
+    Bukkit.getServer().getScheduler().cancelTasks(plugin);
   }
 
+  private void repeatingTask() {
+    // Any game that uses timers or repeating tasks I suggest to use this setup to make the game pause.
+    if (state.isPaused()) return;
+
+    // TODO: Implement logic
+  }
+
+  // This is a basic method that allows players to always be up to date with the latest scoreboard.
+  // Call this method whenever a scoreboard update is required.
   private void refreshScoreboard() {
     Scoreboard scoreboard = getScoreboard();
 
@@ -58,6 +83,7 @@ public class Game implements Listener {
   }
 
   public Scoreboard getScoreboard() {
+    // TODO: most games will require a scoreboard of somesort. This is an example of how to make team prefixes
     Scoreboard scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
 
     Objective obj = scoreboard.registerNewObjective("scoreboard", "scoreboard", "Scoreboard display name");
@@ -74,7 +100,7 @@ public class Game implements Listener {
 
   @EventHandler
   public void onPlayerDeath(PlayerDeathEvent e) {
-    String pName = e.getEntity().getName();
+    state.stopGame(); // this causes the game to end. It will also call the cleanUp() method.
   }
 
   @EventHandler
--- src/main/java/beansplusplus/TemplatePlugin.java
+++ src/main/java/beansplusplus/TemplatePlugin.java
@@ -1,41 +1,58 @@
 package beansplusplus;
 
-import beansplusplus.gameconfig.ConfigLoader;
+import beansplusplus.beansgameplugin.*;
 import net.md_5.bungee.api.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TemplatePlugin extends JavaPlugin implements CommandExecutor, Listener {
-  private Game game;
+import java.io.InputStream;
+import java.util.List;
 
+// plugin entrypoint - implements GameCreator so that it can be used to create games
+public class TemplatePlugin extends JavaPlugin implements GameCreator {
+
+  // this is the basic set up for a game plugin.
   public void onEnable() {
-    ConfigLoader.loadFromInput(getResource("config.yml"));
-    getServer().getPluginManager().registerEvents(this, this);
-    getCommand("start").setExecutor(this);
+    BeansGamePlugin beansGamePlugin = (BeansGamePlugin) getServer().getPluginManager().getPlugin("BeansGamePlugin");
+    beansGamePlugin.registerGame(this); // this method returns the game state if it's required outside of a running game
   }
 
   @Override
-  public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-    if (game != null) {
-      game.end();
-    }
-    game = new Game(this);
-    game.start();
+  public Game createGame(GameConfiguration config, GameState state) {
+    // TODO: Method that creates an instance of the game of your choice. Must implement 'Game'.
+    return new TemplateGame(this, config, state);
+  }
 
+  @Override
+  public boolean isValidSetup(CommandSender sender, GameConfiguration config) {
+    // TODO This should return false and show a message to the command sender if the current config/setup is not satisfactory
+    // just make this method return true if there is no validation required.
+    if (Bukkit.getOnlinePlayers().size() < 2) {
+      sender.sendMessage(ChatColor.DARK_RED + "Required 2 players to start");
+      return false;
+    }
     return true;
   }
 
-  @EventHandler
-  public void onPlayerJoin(PlayerJoinEvent e) {
-    Player player = e.getPlayer();
-
-    player.sendMessage(ChatColor.BLUE + "Welcome to <Template game>");
+  @Override
+  public InputStream config() {
+    // points to a yml of the config
+    return getResource("config.yml");
   }
 
+  @Override
+  public List<String> rulePages() {
+    // TODO: rule book. Can feature ChatColors if needed
+    return List.of(
+        "Rules on page 1",
+        ChatColor.BLUE + "Rules on page 2"
+    );
+  }
+
+  @Override
+  public String name() {
+    // TODO change the name here
+    return "Template game";
+  }
 }
